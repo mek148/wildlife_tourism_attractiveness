cd8dc8659c4f39402c2feb242942fd98d1903098
mek148 <mairekirkland@gmail.com>
2024-02-20 17:31:55 +0000

    Preparing model for batch SLURM job

--- MCMCbird-models.R
+++ MCMCbird-models.R
@@ -13,8 +13,10 @@ rm(list = ls())
 
 ######################### PREPARE DATA #########################
 
+taskID <- commandArgs(trailingOnly = TRUE)
+
 # Imputed bird dataset 
-birdData <- read.csv(paste0(getwd(),"/data/imputed_dataset 1 .csv"))
+birdData <- read.csv(paste0(getwd(),"/data/imputed_dataset ",taskID," .csv"))
 
 # Bird pylogenies
 sampleTrees <- read.tree(paste0(getwd(),"/data/sample_100.tre"))
@@ -64,8 +66,7 @@ prior <- list(R = list(V = diag(2), nu = 0.002, fix = 2),
 # Run multiple chains using 100 different trees and five imputed datasets to obtain 500 models incorporating phylogenetic uncertainty and uncertainty in the imputation procedure. Zero-altered Poisson to account for large number of zeros. 
 set.seed(1) 
 
-zapMCMClist <- mclapply(1:5, function (j) {
-  mclapply(1:100, function(i) {
+zapMCMClist <- mclapply(1:100, function(i) {
     MCMCglmm(
       source ~ trait *
         (
@@ -91,93 +92,26 @@ zapMCMClist <- mclapply(1:5, function (j) {
   },
   
   mc.cores = 100)
-  
-})
 
 # Save models
-save.image("MCMCbird-models.RData")
-
-####################### MODEL CHECKING #########################
-
-# Extract fixed effects
-zapMCMCsol <- lapply(zapMCMClist, function(m) m$Sol)
-zapMCMCsol <- lapply(zapMCMCsol, function(m) m[,1:42]) ## remove random effect means
-zapMCMCsol <- do.call(mcmc.list, zapMCMCsol)
-
-# Extract variances
-zapMCMCvcv <- lapply(zapMCMClist, function(m) m$VCV)
-zapMCMCvcv <- lapply(zapMCMCvcv, function(m) m[,1:5]) ## Remove residual variance component of the binary model, which we fixed to 1
-zapMCMCvcv <- do.call(mcmc.list, zapMCMCvcv)
-
-# Check convergence of chains
-diag.sol <- gelman.diag(zapMCMCsol)  
-diag.vcv <- gelman.diag(zapMCMCvcv)  
-## The closer this factor is to 1, the better the convergence of our chains. In practice, values below 1.1 can be acceptable and values below 1.02 are good
-
-# Check liability for binary process (second part of matrix)
-zapMCMCliab <- lapply(zapMCMClist, function(m) m$Liab)
-zapMCMCliab <- do.call(mcmc.list, zapMCMCliab) # Cnvert to single chain
-
-ul <- 25
-ll <- -25
-
-liab <- lapply(1, function (i) { sum(zapMCMCliab[[i]][,(ncol(zapMCMCliab[[i]])/2+1):ncol(zapMCMCliab[[i]])] > ll & zapMCMCliab[[i]][,(ncol(zapMCMCliab[[i]])/2+1):ncol(zapMCMCliab[[i]])] < ul)/length(zapMCMCliab[[i]][,(ncol(zapMCMCliab[[i]])/2+1):ncol(zapMCMCliab[[i]])]) })
-## 100% of liability estimates fall between -25 and 25, suggesting no numerical problems (see Jarrod's email)
-
-# Plot traces of fixed effects and variances
-plotSOL <- plot(zapMCMCsol, ask = F) 
-plotVCV <- plot(zapMCMCvcv, ask = F) 
-dev.off()
-
-# Check effective sample sizes are adequate (see above)
-effSOL <- effectiveSize(zapMCMCsol) # Fixed effects 
-effVCV <- effectiveSize(zapMCMCvcv[,1:4]) # Phylo random effects
-## Most effective sample sizes are close to 1000, or big enough that >1000 could be achieved with a longer run
-
-# Test for autocorrelation. Values below 0.1 are good.
-autocorr.VCV <- autocorr.diag(zapMCMCvcv)
-autocorr.Sol <- autocorr.diag(zapMCMCsol)
-
-# Function to calculate heritability (phylogenetic signal)
-herit_P <- function(x){
-  (x$VCV[,1]/((x$VCV[,1]+x$VCV[,3])))
-} ## Poisson part
-
-herit_Z <- function(x){
-  (x$VCV[,2]/((x$VCV[,2]+x$VCV[,4])+1))
-} ## Zero inflation part
-
-# Report mean heritability for each part of the model
-her_P <- mean(as.numeric((lapply(1:2, function(i) mean(herit_P(zapMCMClist[[i]])))))) 
-her_Z <- mean(as.numeric((lapply(1:2, function(i) mean(herit_Z(zapMCMClist[[i]]))))))
-## These are both fairly high, which I think is encouraging as we should expect a phylogenetic signal in the data
-
-# Model summary
-summVCV <- summary(zapMCMCvcv) ## Variance
-summSOL <- summary(zapMCMCsol) ## Fixed effects
-
-######################## PREDICTIONS #########################
+saveRDS(zapMCMClist, paste0("MCMCbird-models",taskID,".rds"))
 
 # Model predictions
-predictions <- mclapply(1, function (x) { predict(zapMCMClist[[x]], marginal = NULL, 
-          type = "response", posterior = "mean") }, mc.cores = 2)
+predictions <- mclapply(1:100, function (j) { predict(zapMCMClist[[j]], marginal = NULL, 
+          type = "response", posterior = "mean") }, mc.cores = 8)
 
 # Fix political stability constant at maximum
 newData <- birdData
 newData$scaled_ps <- max(newData$scaled_ps)*rnorm(nrow(newData), 1, 1e-5)
 
-fixed.predictions <- mclapply(1:500, function (i) { 
-  predict(zapMCMClist[[i]], marginal = NULL, newdata = newData, 
-          type = "response", posterior = "mean") }, mc.cores = 2) 
+fixed.predictions <- mclapply(1:100, function (k) { 
+  predict(zapMCMClist[[k]], marginal = NULL, newdata = newData, 
+          type = "response", posterior = "mean") }, mc.cores = 8) 
 
 # Create data frames of predictions
 predictionsDF <- as.data.frame(t(do.call(rbind.data.frame, Map('c', predictions))))
 fixedDF <- as.data.frame(t(do.call(rbind.data.frame, Map('c', fixed.predictions))))
 
 # Export
-write.csv(predictionsDF, "bird-predictions.csv", row.names = F)
-write.csv(fixedDF, "bird-fixed-predictions.csv", row.names = F)
-
-# Save image without large list of models
-rm(zapMCMClist)
-save.image("MCMCbird-results.RData")
+write.csv(predictionsDF, paste0("bird-predictions",taskID,".csv"), row.names = F)
+write.csv(fixedDF, paste0("bird-fixed-predictions",taskID,".csv"), row.names = F)
