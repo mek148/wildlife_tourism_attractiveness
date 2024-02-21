7df6fc33a73693f42fad01d277589bc4c42fa70b
mek148 <mairekirkland@gmail.com>
2024-02-21 12:50:23 +0000

    update R script

--- test.R
+++ test.R
@@ -2,11 +2,12 @@
 ## Testing sbatch script on multiple datasets and multiple trees ##
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-# Index for dataset and phylogenetic tree
 args <- commandArgs(T)
 
 print(args[1])
 
-df <- read.csv(paste0(args[1],".csv"))
+df <- read.csv(paste0(getwd(),"/data/",args[1],".csv"))
 
 head(df)
+
+write.csv(paste0(getwd(),"/data/new-",args[1],".csv"))
