5d1c48fbc991f3cbbecbaad06b40b34ffe8a7ffe
mek148 <mairekirkland@gmail.com>
2024-02-21 12:52:14 +0000

    fix write.csv

--- test.R
+++ test.R
@@ -10,4 +10,4 @@ df <- read.csv(paste0(getwd(),"/data/",args[1],".csv"))
 
 head(df)
 
-write.csv(paste0(getwd(),"/data/new-",args[1],".csv"))
+write.csv(df, paste0(getwd(),"/data/new-",args[1],".csv"))
