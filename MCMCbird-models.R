5b67b5dbf910a8e90836aab8d867596f9bfba262
mek148 <mairekirkland@gmail.com>
2024-02-28 09:12:26 +0000

    fix read in of individual tree

--- MCMCbird-models.R
+++ MCMCbird-models.R
@@ -16,7 +16,7 @@ rm(list = ls())
 # Command argument for dataset and phylogenetic tree, as you are looping through 5 datasets and 100 trees
 args <- commandArgs(T)
 species.df <- read.csv(paste0(getwd(),"/data/",args[1],".csv"))
-tree <- read.tree(paste0(getwd(),"/data/sample_100.tre"))[[args[2]]]
+tree <- read.tree(paste0(getwd(),"/data/sample_100.tre"))[args[2]]
 
 # Convert extinction risk and migration to continuous variable
 species.df$ext_risk <- ifelse(species.df$status == "LC", 1, ifelse(species.df$status == "NT", 2, 
